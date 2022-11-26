cf0527eca19992c12b0bf40c3bac5902cd9cf2df
Guilherme <guilhermeguiguilima001@gmail.com>
2022-11-26 16:51:55 -0300

    first commit

---
+++ src/banco/Main.java
@@ -0,0 +1,19 @@
+package banco;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Guilherme");
+		
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
+
+		cc.depositar(200);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+
+}
